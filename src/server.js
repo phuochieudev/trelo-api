5a1c49feff6878b676b19f6911e0be3453e4dd21
phuochieu <phieeus99@gmail.com>
2023-10-25 13:40:21 +0700

    Init Code

--- src/server.js
+++ src/server.js
@@ -1,31 +1,43 @@
-/**
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
+const START_SERVER = () => {
+  const app = express()
 
-const app = express()
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-const hostname = 'localhost'
-const port = 8017
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR}, Back-end Server is running successfully at Host: ${ env.APP_HOST } and Port ${ env.APP_PORT }`)
+  })
+  //Thực hiện các tác vụ cleanup trước khi dừng server
+  exitHook( () => {
+    CLOSE_DB()
+    console.log('Disconnected from MongoDB Cloud Atlas')
+  })
+}
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+(async () => {
+  try {
+    await CONNECT_DB()
+    console.log('Connected to MongoDB Cloud Atlas')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+})()
+
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlas'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
+
