e8487fdef12ba424358fcb85c751eb12054b4083
phuochieu <phieeus99@gmail.com>
2023-11-06 14:16:35 +0700

    Init Code

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -19,11 +20,9 @@ const createNew = async (req, res, next) => {
     //Validate dữ liệu xong, hợp lệ thì cho request đi tiếp sang Controller
     next()
   } catch (error) {
-    console.log(error)
-    //console.log(new Error(error))
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    // const errorMessage = new Error(error).message
+    // const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 export const boardValidation = {
