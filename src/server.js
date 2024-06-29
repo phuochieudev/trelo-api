fd7303015d8202f2f94aa040d8dc82ac4c0a1184
phuochieu <phieeus99@gmail.com>
2024-06-29 16:12:34 +0700

    fix master 2

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
-import {errorHandlingMiddleware} from '~/middlewares/errorHandlingMiddleware'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
+
 const START_SERVER = () => {
   const app = express()
   //Enable req.body json data
@@ -12,6 +13,10 @@ const START_SERVER = () => {
 
   //Use API v1
   app.use('/v1', APIs_V1)
+
+  //Middleware xu ly loi tap trung
+  app.use(errorHandlingMiddleware)
+
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
     console.log(`Hello ${env.AUTHOR}, Back-end Server is running successfully at Host: ${ env.APP_HOST } and Port ${ env.APP_PORT }`)
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -17,13 +16,14 @@ const createNew = async (req, res, next) => {
   try {
     //Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-    //next()
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API creat new board' })
-
+    //Validate dữ liệu xong, hợp lệ thì cho request đi tiếp sang Controller
+    next()
   } catch (error) {
-    // const errorMessage = new Error(error).message
-    // const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
-    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+    console.log(error)
+    //console.log(new Error(error))
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
   }
 }
 export const boardValidation = {
