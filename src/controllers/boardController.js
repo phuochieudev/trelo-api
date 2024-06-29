82b9f9e7cfe5f0fd192169809f174d381276917b
phuochieu <phieeus99@gmail.com>
2024-06-29 17:56:52 +0700

    Code Middleware

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,19 +1,20 @@
 import { StatusCodes } from 'http-status-codes'
 const createNew = async (req, res, next) => {
   try {
-    // console.log('req.body:', req.body)
-    // console.log('req.query:', req.query)
-    // console.log('req.params:', req.params)
-    // console.log('req.files:', req.files)
-    // console.log('req.cookies:', req.cookies)
-    // console.log('req.jwtDecoded:', req.jwtDecoded)
+    console.log('req.body:', req.body)
+    console.log('req.query:', req.query)
+    console.log('req.params:', req.params)
+    console.log('req.files:', req.files)
+    console.log('req.cookies:', req.cookies)
+    console.log('req.jwtDecoded:', req.jwtDecoded)
 
+    // throw new Error('phuochieudev test')
     //Điều hướng dữ liệu sang tần Service
     //Có kết quả thì trả về phía client
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API creat new board' })
   } catch (error) {
-   next(error)
-    }
+    next(error)
+  }
 }
 
 export const boardController = {
